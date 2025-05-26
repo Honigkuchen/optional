272cbf146fce1e56a9febb06649bb7695367f8df
Steve Downey <sdowney@gmail.com>
2025-05-26 15:32:56 -0400

    Add llvm coverage tool

--- etc/clang-21-toolchain.cmake
+++ etc/clang-21-toolchain.cmake
@@ -7,5 +7,6 @@ include_guard(GLOBAL)
 
 set(CMAKE_C_COMPILER clang-21)
 set(CMAKE_CXX_COMPILER clang++-21)
+set(GCOV_EXECUTABLE "llvm-cov-21 gcov" CACHE STRING "GCOV executable" FORCE)
 
 include("${CMAKE_CURRENT_LIST_DIR}/clang-flags.cmake")
